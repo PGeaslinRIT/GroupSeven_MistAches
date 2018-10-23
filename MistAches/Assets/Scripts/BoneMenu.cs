9b60338ccca4a13fe56afe5ffe335576ca79074c
Allie <axs9711@rit.edu>
2018-10-23 15:19:25 -0400

    A - began work on bone screen and state machine
    implemented a bone screen which shows up in the sandbox when tab is pressed, added a scene for the main menu and game over, and began work on changin states between these screens

--- MistAches/Assets/Scripts/BoneMenu.cs
+++ MistAches/Assets/Scripts/BoneMenu.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class BoneMenu : MonoBehaviour {
 
+	public GameObject panel;
+	private bool on;
+
 	// Use this for initialization
 	void Start () {
-		
+		on = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Tab)) {
+			on = !on;
+			panel.SetActive (on);
+		}
 	}
 }
--- MistAches/Assets/Scripts/GameStateController.cs
+++ MistAches/Assets/Scripts/GameStateController.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateController : MonoBehaviour {
 
+	public GameObject player;
+	public GameObject button;
+	public int state;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (gameObject.scene.name == "mainmenu")
+			state = 1;
+		else if (gameObject.scene.name == "gameover")
+			state = 2;
+		else if (gameObject.scene.name == "sandbox")
+			state = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		switch (state) {
+		//main menu
+		case 1:
+			break;
+		//game over
+		case 2:
+			break;
+		//pause
+		case 3:
+			break;
+		//level 1
+		case 4:
+			break;
+		case 10:
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+				state = 3;
+			}
+			if (player.transform.position.y <= -10) {
+				state = 2;
+			}
+			break;
+		}
 	}
 }
