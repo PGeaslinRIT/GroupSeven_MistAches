caf6f3149904882618d2393895fc024ed3b5648e
PGeaslinRIT <pxg6350@rit.edu>
2018-10-30 13:35:12 -0400

    P - Basic Climbing pt2 still untested

--- MistAches/Assets/Scripts/Climb.cs
+++ MistAches/Assets/Scripts/Climb.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Climb : MonoBehaviour {
 	private List<GameObject> plantObjList;
 	private BoxCollider2D playerCollider;
+	private Vector3 position;
 
 	public float climbSpeed = 0.05f;
 
@@ -14,17 +15,21 @@ public class Climb : MonoBehaviour {
 	void Start () {
 		plantObjList = GameObject.Find ("LevelManager").GetComponent<ReferenceManager>().plantObjList;
 
-		playerCollider.GetComponent<BoxCollider2D> ();
+		playerCollider = GetComponent<BoxCollider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		position = gameObject.transform.position;
+
 		if (Input.GetKeyUp(KeyCode.W)) {
 			foreach (GameObject obj in plantObjList) {
 				if (playerCollider.IsTouching(obj.GetComponent<BoxCollider2D> ())) {
-					gameObject.transform.position.y += climbSpeed;
+					position.y += climbSpeed;
 				}
 			}
+
+			gameObject.transform.position = position;
 		}
 	}
 }
