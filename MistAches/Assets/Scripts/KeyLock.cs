3ae024c92121a93c084683e7b0730a8dc56fe421
Allie <axs9711@rit.edu>
2018-10-30 13:32:19 -0400

    A - began work on Player holding key

--- MistAches/Assets/Scripts/KeyLock.cs
+++ MistAches/Assets/Scripts/KeyLock.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class KeyLock : MonoBehaviour {
 
+	private ReferenceManager refManager;
+
 	public List<GameObject> keys;
 	public List<GameObject> locks;
 
+	private GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+		refManager = GetComponent<ReferenceManager> ();
+
+		player = refManager.playerObj;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		for (int i = 0; i < keys.Count; i++) {
+			if (player.GetComponent<BoxCollider2D> ().IsTouching (keys [i].GetComponent<BoxCollider2D> ())) {
+				Destroy (keys [i]);
+				keys.RemoveAt (i);
+			}
+		}
+
 		for (int i = 0; i < keys.Count; i++) {
 			for (int j = 0; j < locks.Count; j++) {
 				if (keys [i].GetComponent<BoxCollider2D> ().IsTouching (locks [i].GetComponent<BoxCollider2D> ())) {
