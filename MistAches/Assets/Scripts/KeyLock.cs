8c14dc695430fab358cbad35d5806a791ca55cc2
Allie <axs9711@rit.edu>
2018-10-30 12:02:16 -0400

    A - Started key/lock stuff

---
+++ MistAches/Assets/Scripts/KeyLock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLock : MonoBehaviour {
+
+	public List<GameObject> keys;
+	public List<GameObject> locks;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
