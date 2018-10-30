ff0635e40b8e6ae11597c56f05dfd084b2e36fc9
PGeaslinRIT <pxg6350@rit.edu>
2018-10-30 12:52:16 -0400

    P - Plant Visuals
    Plant grows with rain and heat

---
+++ MistAches/Assets/Scripts/PlantGrowth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantGrowth : MonoBehaviour {
+	private WeatherController weatherControllerObj;
+
+	public Sprite fullGrownSprite;
+	public bool isGrown = false;
+
+	// Use this for initialization
+	void Start () {
+		weatherControllerObj = GameObject.Find ("LevelManager").GetComponent<WeatherController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isGrown && weatherControllerObj.IsHot () && weatherControllerObj.IsRaining ()) {
+			GetComponent<SpriteRenderer> ().sprite = fullGrownSprite;
+			Destroy(GetComponent<BoxCollider2D>());
+			gameObject.AddComponent<BoxCollider2D> ();
+		}
+	}
+}
