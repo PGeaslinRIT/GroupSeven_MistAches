f29b5511ada816ddc87e508ec023013ef472363d
Allie <axs9711@rit.edu>
2018-10-30 12:54:35 -0400

    A - deletes key now too

--- MistAches/Assets/Scripts/KeyLock.cs
+++ MistAches/Assets/Scripts/KeyLock.cs
@@ -17,6 +17,7 @@ public class KeyLock : MonoBehaviour {
 		for (int i = 0; i < keys.Count; i++) {
 			for (int j = 0; j < locks.Count; j++) {
 				if (keys [i].GetComponent<BoxCollider2D> ().IsTouching (locks [i].GetComponent<BoxCollider2D> ())) {
+					Destroy (keys [i]);
 					Destroy (locks [i]);
 				}
 			}
