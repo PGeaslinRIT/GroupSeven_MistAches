549b9b84e9c60e8c38ec02cf6d1466124ed897de
Allie <axs9711@rit.edu>
2018-10-31 14:25:13 -0400

    A - started level 4, lowered wind strength
    Began construction of level 4. Lowered the strength of wind, which fixed an issue where the player could clip through walls by blowing wind to the side.

--- MistAches/Assets/Scripts/WindObj.cs
+++ MistAches/Assets/Scripts/WindObj.cs
@@ -49,16 +49,16 @@ public class WindObj : MonoBehaviour {
 	public Vector3 CalcForce () {
 		switch (myDir) {
 		case Direction.up:
-			myForce.y = 1.0f;
+			myForce.y = .5f;
 			break;
 		case Direction.down:
-			myForce.y = -1.0f;
+			myForce.y = -.5f;
 			break;
 		case Direction.left:
-			myForce.x = -1.0f;
+			myForce.x = .5f;
 			break;
 		case Direction.right:
-			myForce.x = 1.0f;
+			myForce.x = .5f;
 			break;
 		default:
 		case Direction.none:
